857142af5ff08a2f1078d450dea16a749d8ab9ee
JupDev <97832488+JupDiv@users.noreply.github.com>
2023-04-12 19:38:11 +0300

    some refactoring

--- src/components/CharacterDetails/CharacterDetails.tsx
+++ src/components/CharacterDetails/CharacterDetails.tsx
@@ -33,10 +33,8 @@ function CharacterDetails({charaster}: CharacterDetailsProps): JSX.Element {
   const other = useAppSelector(state => state.favouriteCharaster.other);
 
   const isCharacterFavourite = useMemo(() => {
-    return (
-      female.some(favCharName => favCharName === name) ||
-      male.some(favCharName => favCharName === name) ||
-      other.some(favCharName => favCharName === name)
+    return [female, male, other].some(favCharName =>
+      favCharName.includes(name),
     );
   }, [female, male, other, name]);
 
